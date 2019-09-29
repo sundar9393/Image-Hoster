01bee2445a0b5a83dd5baa745b9b77a24e7c75ba
sundar93 <sundar9393@gmail.com>
2019-09-29 23:02:25 +0530

    initial commit

---
+++ ImageHoster/src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,19 @@
+package ImageHoster.service;
+
+import ImageHoster.model.Comment;
+import ImageHoster.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    CommentRepository commentRepository;
+
+    private CommentService() {
+    }
+
+    public void addComment(Comment comment){
+        commentRepository.addComment(comment);
+    }
+}
